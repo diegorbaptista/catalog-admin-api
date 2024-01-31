5300c7e281b134cc12e10a3956b84fe413fca181
Diego Baptista <diego.rbaptista@gmail.com>
2024-01-30 23:02:54 -0300

    implementing category validations and tests

--- domain/src/main/java/com/codemagic/catalog/admin/domain/category/CategoryValidator.java
+++ domain/src/main/java/com/codemagic/catalog/admin/domain/category/CategoryValidator.java
@@ -15,5 +15,11 @@ public class CategoryValidator extends Validator {
         if (this.category.getName() == null) {
             this.handler().append(new Error("'name' should not be null"));
         }
+        if (this.category.getName().isBlank()) {
+            this.handler().append(new Error("'name' should not be empty"));
+        }
+        if ((this.category.getName().trim().length() < 3) || (this.category.getName().trim().length() > 255)) {
+            this.handler().append(new Error("'name' length must be between 3 and 255 characters"));
+        }
     }
 }
--- domain/src/test/java/com/codemagic/catalog/admin/domain/CategoryTest.java
+++ domain/src/test/java/com/codemagic/catalog/admin/domain/CategoryTest.java
@@ -35,4 +35,51 @@ public class CategoryTest {
         assertEquals(expectedException.getErrors().size(), expectedErrorCount);
     }
 
+    @Test
+    void givenAnInvalidEmptyName_whenCallNewCategoryAndValidate_thenShouldReturnAnError() {
+        final String expectedName = "    ";
+        final String expectedDescription = "Most watched movies";
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription);
+        final var expectedException = assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        assertEquals(expectedException.getErrors().get(0).message(), expectedErrorMessage);
+        assertEquals(expectedException.getErrors().size(), expectedErrorCount);
+    }
+    @Test
+    void givenAnInvalidNameGreaterThan255Characters_whenCallNewCategoryAndValidate_thenShouldReturnAnError() {
+        final String expectedName = """
+                t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+                Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like.
+                """;
+        final String expectedDescription = "Most watched movies";
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' length must be between 3 and 255 characters";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription);
+        final var expectedException = assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        assertEquals(expectedException.getErrors().get(0).message(), expectedErrorMessage);
+        assertEquals(expectedException.getErrors().size(), expectedErrorCount);
+    }
+
+    @Test
+    void givenAValidEmptyDescription_whenCreatingANewCategory_thenCreateAValidCategory() {
+        var expectedName = "Movies";
+        var expectedDescription = "   ";
+        var category = Category.newCategory(expectedName, expectedDescription);
+
+        assertDoesNotThrow(() -> category.validate(new ThrowsValidationHandler()));
+        assertEquals(expectedName, category.getName());
+        assertEquals(expectedDescription, category.getDescription());
+        assertTrue(category.isActive());
+        assertNotNull(category.getCreatedAt());
+        assertNotNull(category.getUpdatedAt());
+        assertNull(category.getDeletedAt());
+    }
+
 }
