2516cc061f0811b242842424df8b8f8a95d5d2a6
Diego Baptista <diego.rbaptista@gmail.com>
2024-02-03 10:07:41 -0300

    add category gateway inteface

---
+++ domain/src/main/java/com/codemagic/catalog/admin/domain/category/CategoryGateway.java
@@ -0,0 +1,13 @@
+package com.codemagic.catalog.admin.domain.category;
+
+import com.codemagic.catalog.admin.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+    Category create(final Category category);
+    void deleteById(final CategoryID categoryID);
+    Optional<Category> findById(final CategoryID categoryID);
+    Category update(final Category category);
+    Pagination<Category> findAll(final CategorySearchQuery query);
+}
