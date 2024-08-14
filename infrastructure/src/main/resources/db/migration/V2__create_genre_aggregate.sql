b9a24383e885c3b89ba03327acaad5e15c943409
Diego Baptista <diego.rbaptista@gmail.com>
2024-08-13 22:16:26 -0300

    update identifier length in tables to use char(32)

--- infrastructure/src/main/resources/db/migration/V2__create_genre_aggregate.sql
+++ infrastructure/src/main/resources/db/migration/V2__create_genre_aggregate.sql
@@ -1,5 +1,5 @@
 create table genres (
-    id varchar(36) not null primary key,
+    id char(32) not null primary key,
     name varchar(255) not null,
     active boolean not null,
     created_at datetime(6) not null,
@@ -8,8 +8,8 @@ create table genres (
 );
 
 create table genres_categories (
-    genre_id varchar(36) not null,
-    category_id varchar(36) not null,
+    genre_id char(32) not null,
+    category_id char(32) not null,
     constraint idx_genres_categories unique (genre_id, category_id),
     constraint fk_genres_categories_genre_id foreign key (genre_id) references genres(id) on delete cascade,
     constraint fk_genres_categories_category_id foreign key (category_id) references categories(id) on delete cascade
