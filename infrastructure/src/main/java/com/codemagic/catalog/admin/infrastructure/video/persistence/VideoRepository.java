d14d9e6e7a6431319c45909b691abeae0e2d8f06
Diego Baptista <diego.rbaptista@gmail.com>
2024-08-20 21:37:59 -0300

    rename video jpa repository

--- infrastructure/src/main/java/com/codemagic/catalog/admin/infrastructure/video/persistence/VideoRepository.java
+++ infrastructure/src/main/java/com/codemagic/catalog/admin/infrastructure/video/persistence/VideoRepository.java
@@ -2,5 +2,5 @@ package com.codemagic.catalog.admin.infrastructure.video.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VideoJpaRepository extends JpaRepository<VideoJpaEntity, String> {
+public interface VideoRepository extends JpaRepository<VideoJpaEntity, String> {
 }
